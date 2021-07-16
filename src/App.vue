b3e5b6d843a55bdd29d038770c93b2cbc7107f64
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-07-16 10:12:34 +0300

    Create and design TheNavbar component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <TheNavbar />
+  <router-view />
 </template>
 
+<script>
+import TheNavbar from "./components/TheNavbar";
+
+export default {
+  name: "App",
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// Imports
+@import "./styles/variables";
+
+// Reset
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
 }
 
-#nav {
-  padding: 30px;
+ul {
+  list-style-type: none;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+a {
+  text-decoration: none;
 }
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <header class="navbar">
+    <a
+      href="/"
+      class="logo"
+    >Rick and Morty Characters</a>
+
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+// Imports
+@import "../styles/variables";
+
+header {
+  width: 100%;
+  height: 100px;
+  padding: 0 $px;
+  background-color: $color-primary;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    width: 200px;
+    color: $color-light;
+    font-size: 22px;
+  }
+
+  nav ul {
+    display: flex;
+    justify-content: space-between;
+
+    li:not(:last-child) {
+      margin-right: 20px;
+    }
+
+    li a {
+      color: $color-secondary;
+      font-weight: bold;
+
+      &.router-link-active {
+        border-bottom: 2px solid $color-light;
+      }
+    }
+  }
+}
+
+// Media Queries
+@media (max-width: 768px) {
+  header .logo {
+    font-size: 18px;
+  }
+}
+</style>
